876eebc28e6067600c4d2c1fd20eca80ff212775
zjacobsdev <z.jacobsdev@gmail.com>
2020-03-05 20:28:01 -0500

    Daily CodeWar 3/5/2020

---
+++ string_end_with_7kyu/main.js
@@ -0,0 +1,26 @@
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+
+*/
+function solution(str, ending){
+
+   result = str.endsWith(ending)
+
+    // TODO: complete
+  }
+
+
+  solution('abcde', 'cde')//, true)
+solution('abcde', 'abc')//, false)
+
+
+
+
+
+
