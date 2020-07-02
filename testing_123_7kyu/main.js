7a78d636510a246e88954f6af68957e14292a62f
zjacobsdev <z.jacobsdev@gmail.com>
2020-07-02 00:41:49 -0400

    7/2/20_SOLVED

--- testing_123_7kyu/main.js
+++ testing_123_7kyu/main.js
@@ -3,22 +3,37 @@
 Write a function which takes a list of strings and returns each line prepended by the correct number.
 
 The numbering starts at 1. The format is n: string. Notice the colon and space in between.
-
+ 
 Examples:
 
 number([]) // => []
-number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"] */
+number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"] 
+
+
+IMPROVE:
+ use map instead of creating new array
+*/
 
 var number = function (array) {
 
-  newArr = []
+  newArr = []   
 
-  for (i=0; i<= array.length; i++){
+  if(array.length){
+
+   for (i=0; i < array.length; i++){
 
     newArr.push(`${i+1}: ${array[i]}`)
 
-  }
+    }
+
+   return newArr
   //your awesome code here
+  }else{
+
+    return []
+  }
+
+ 
 };
 
 number([]); //, [], 'Empty array should return empty array');
