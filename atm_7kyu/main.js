16488ab713899a3437180c6c6653570a7e738bbb
zjacobsdev <z.jacobsdev@gmail.com>
2020-07-16 15:05:41 -0400

    7/16/2020 7kyu SOLVED

--- atm_7kyu/main.js
+++ atm_7kyu/main.js
@@ -13,37 +13,55 @@ Good Luck!!!
 function solve(n) {
     //let minNotesFound = true
 
+    if(n % 10 != 0){
+        console.log(-1)
+        return -1
+    }
 
-   let listOfNomValues = [ 10, 20 ,50, 100,500]
-   let countOfNotes
+
+   let listOfNomValues = [ 10, 20 ,50, 100,200,500]
+   let countOfNotes = 0
 
 
+    for(let i = listOfNomValues.length - 1; i >= 0; i--){
 
 
-    for(let i = listOfNomValues.length; i >= 0; i--){
+        console.log(`%c ${listOfNomValues[i]}`, `color: orange;`)
 
-        if (n % listOfNomValues[i] == 0){
+        if(n < listOfNomValues[i]){ continue;}
 
-            return n/listOfNomValues [i]
-
+        if (n % listOfNomValues[i] === 0){ 
+            
+            countOfNotes = countOfNotes + n/listOfNomValues [i] 
+             break
+        
         }
 
-        currentCountOfNotes = Math.round(n / i) 
-       let amtTaken = countOfNotes * listOfNomValues[i] 
-
-       let amtLeft = n - amtTaken 
-       n = amtLeft 
-
+        let currentCountOfNotes = Math.floor(n / listOfNomValues[i]) 
 
         countOfNotes = countOfNotes + currentCountOfNotes 
 
+        console.log(`%c ${currentCountOfNotes}`, 'color: green;')
+       // console.log(countOfNotes)
+        
 
+       let amtTaken = currentCountOfNotes * listOfNomValues[i] 
 
+       console.log(amtTaken)
+
+       let amtLeft = n - amtTaken 
+
+       console.log(`%c ${amtLeft}`,`color:red;`)
+       n = amtLeft 
+        console.log(n)
+        console.log(`%c ${countOfNotes}`, `color: yellow`)
     }
 
-    console.log(countOfNotes)
+    console.log(`%c ${countOfNotes}`, `color: yellow`)
 
+    //console.log(countOfNotes)
 
+    
   }
 
 
@@ -60,12 +78,12 @@ function solve(n) {
 
 
 
-  solve(770)//, 4, "Wrong result for 770");
-  solve(550)//, 2, "Wrong result for 550");
-  solve(10)//, 1, "Wrong result for 10");
+//  solve(770)//, 4, "Wrong result for 770");
+//   solve(550)//, 2, "Wrong result for 550");
+//   solve(10)//, 1, "Wrong result for 10");
    
-  solve(1250)//, 4, "Wrong result for 1250
+ solve(1250)//, 4, "Wrong result for 1250
 
-  solve(125)//, -1, "Wrong result for 125");
-  solve(666)//, -1, "Wrong result for 666");
-solve(42)//, -1, "Wrong result for 42");
+//  solve(125)//, -1, "Wrong result for 125");
+ // solve(666)//, -1, "Wrong result for 666");
+//solve(42)//, -1, "Wrong result for 42");
